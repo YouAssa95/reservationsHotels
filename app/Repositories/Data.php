f5e4c03b120d629737008d402501ac37b8004d14
EL OUARRAT Youssef <youssef.el-ouarrat@etu.univ-amu.fr>
2021-02-18 11:08:11 +0100

    delete

--- app/Repositories/Data.php
+++ app/Repositories/Data.php
@@ -7,51 +7,5 @@ class Data
 {
     
 
-    function matches()
-    {
-       
 
-    function expectedMatchesForTeam4()
-    {
-        return [
-            ['id' => 7, 'team0' => 4, 'team1' => 18, 'score0' => 3, 'score1' => 5, 'date' => '2048-08-03 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Angers'],
-            ['id' => 18, 'team0' => 14, 'team1' => 4, 'score0' => 0, 'score1' => 1, 'date' => '2048-08-10 00:00:00', 'name0' => 'Gap', 'name1' => 'Toulouse'],
-            ['id' => 29, 'team0' => 4, 'team1' => 2, 'score0' => 0, 'score1' => 3, 'date' => '2048-08-17 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Marseille'],
-            ['id' => 40, 'team0' => 1, 'team1' => 4, 'score0' => 2, 'score1' => 3, 'date' => '2048-08-24 00:00:00', 'name0' => 'Paris', 'name1' => 'Toulouse'],
-            ['id' => 50, 'team0' => 6, 'team1' => 4, 'score0' => 4, 'score1' => 1, 'date' => '2048-08-31 00:00:00', 'name0' => 'Nantes', 'name1' => 'Toulouse'],
-            ['id' => 59, 'team0' => 4, 'team1' => 10, 'score0' => 4, 'score1' => 4, 'date' => '2048-09-07 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Lille'],
-            ['id' => 68, 'team0' => 15, 'team1' => 4, 'score0' => 4, 'score1' => 4, 'date' => '2048-09-14 00:00:00', 'name0' => 'Toulon', 'name1' => 'Toulouse'],
-            ['id' => 77, 'team0' => 4, 'team1' => 13, 'score0' => 0, 'score1' => 0, 'date' => '2048-09-21 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Le Havre'],
-            ['id' => 86, 'team0' => 16, 'team1' => 4, 'score0' => 1, 'score1' => 5, 'date' => '2048-09-28 00:00:00', 'name0' => 'Grenoble', 'name1' => 'Toulouse'],
-            ['id' => 95, 'team0' => 4, 'team1' => 11, 'score0' => 2, 'score1' => 0, 'date' => '2048-10-05 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Rennes'],
-            ['id' => 104, 'team0' => 12, 'team1' => 4, 'score0' => 4, 'score1' => 5, 'date' => '2048-10-12 00:00:00', 'name0' => 'Reims', 'name1' => 'Toulouse'],
-            ['id' => 113, 'team0' => 4, 'team1' => 3, 'score0' => 5, 'score1' => 3, 'date' => '2048-10-19 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Lyon'],
-            ['id' => 122, 'team0' => 5, 'team1' => 4, 'score0' => 1, 'score1' => 0, 'date' => '2048-10-26 00:00:00', 'name0' => 'Nice', 'name1' => 'Toulouse'],
-            ['id' => 131, 'team0' => 4, 'team1' => 7, 'score0' => 4, 'score1' => 5, 'date' => '2048-11-02 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Strasbourg'],
-            ['id' => 142, 'team0' => 4, 'team1' => 8, 'score0' => 1, 'score1' => 2, 'date' => '2048-11-09 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Montpellier'],
-            ['id' => 153, 'team0' => 9, 'team1' => 4, 'score0' => 4, 'score1' => 1, 'date' => '2048-11-16 00:00:00', 'name0' => 'Bordeaux', 'name1' => 'Toulouse'],
-            ['id' => 164, 'team0' => 4, 'team1' => 19, 'score0' => 3, 'score1' => 0, 'date' => '2048-11-23 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Nîmes'],
-            ['id' => 175, 'team0' => 17, 'team1' => 4, 'score0' => 3, 'score1' => 2, 'date' => '2048-11-30 00:00:00', 'name0' => 'Dijon', 'name1' => 'Toulouse'],
-            ['id' => 186, 'team0' => 4, 'team1' => 20, 'score0' => 1, 'score1' => 1, 'date' => '2048-12-07 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Villeurbanne'],
-            ['id' => 197, 'team0' => 18, 'team1' => 4, 'score0' => 4, 'score1' => 2, 'date' => '2048-12-28 00:00:00', 'name0' => 'Angers', 'name1' => 'Toulouse'],
-            ['id' => 208, 'team0' => 4, 'team1' => 14, 'score0' => 4, 'score1' => 1, 'date' => '2049-01-04 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Gap'],
-            ['id' => 219, 'team0' => 2, 'team1' => 4, 'score0' => 4, 'score1' => 2, 'date' => '2049-01-11 00:00:00', 'name0' => 'Marseille', 'name1' => 'Toulouse'],
-            ['id' => 230, 'team0' => 4, 'team1' => 1, 'score0' => 5, 'score1' => 0, 'date' => '2049-01-18 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Paris'],
-            ['id' => 240, 'team0' => 4, 'team1' => 6, 'score0' => 5, 'score1' => 4, 'date' => '2049-01-25 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Nantes'],
-            ['id' => 249, 'team0' => 10, 'team1' => 4, 'score0' => 2, 'score1' => 3, 'date' => '2049-02-01 00:00:00', 'name0' => 'Lille', 'name1' => 'Toulouse'],
-            ['id' => 258, 'team0' => 4, 'team1' => 15, 'score0' => 3, 'score1' => 0, 'date' => '2049-02-08 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Toulon'],
-            ['id' => 267, 'team0' => 13, 'team1' => 4, 'score0' => 1, 'score1' => 1, 'date' => '2049-02-15 00:00:00', 'name0' => 'Le Havre', 'name1' => 'Toulouse'],
-            ['id' => 276, 'team0' => 4, 'team1' => 16, 'score0' => 3, 'score1' => 5, 'date' => '2049-02-22 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Grenoble'],
-            ['id' => 285, 'team0' => 11, 'team1' => 4, 'score0' => 2, 'score1' => 5, 'date' => '2049-03-01 00:00:00', 'name0' => 'Rennes', 'name1' => 'Toulouse'],
-            ['id' => 294, 'team0' => 4, 'team1' => 12, 'score0' => 5, 'score1' => 5, 'date' => '2049-03-08 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Reims'],
-            ['id' => 303, 'team0' => 3, 'team1' => 4, 'score0' => 0, 'score1' => 3, 'date' => '2049-03-15 00:00:00', 'name0' => 'Lyon', 'name1' => 'Toulouse'],
-            ['id' => 312, 'team0' => 4, 'team1' => 5, 'score0' => 2, 'score1' => 5, 'date' => '2049-03-22 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Nice'],
-            ['id' => 321, 'team0' => 7, 'team1' => 4, 'score0' => 4, 'score1' => 3, 'date' => '2049-03-29 00:00:00', 'name0' => 'Strasbourg', 'name1' => 'Toulouse'],
-            ['id' => 332, 'team0' => 8, 'team1' => 4, 'score0' => 0, 'score1' => 5, 'date' => '2049-04-05 00:00:00', 'name0' => 'Montpellier', 'name1' => 'Toulouse'],
-            ['id' => 343, 'team0' => 4, 'team1' => 9, 'score0' => 0, 'score1' => 1, 'date' => '2049-04-12 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Bordeaux'],
-            ['id' => 354, 'team0' => 19, 'team1' => 4, 'score0' => 2, 'score1' => 3, 'date' => '2049-04-19 00:00:00', 'name0' => 'Nîmes', 'name1' => 'Toulouse'],
-            ['id' => 365, 'team0' => 4, 'team1' => 17, 'score0' => 5, 'score1' => 3, 'date' => '2049-04-26 00:00:00', 'name0' => 'Toulouse', 'name1' => 'Dijon'],
-            ['id' => 376, 'team0' => 20, 'team1' => 4, 'score0' => 1, 'score1' => 3, 'date' => '2049-05-03 00:00:00', 'name0' => 'Villeurbanne', 'name1' => 'Toulouse']
-        ];
-    }
 }
